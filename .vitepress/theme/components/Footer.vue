a021a1a582fee65331ab14831a76035ce0d42cec
sunnyboy <447376177@qq.com>
2026-01-31 21:00:09 +0800

    Update Footer.vue

--- .vitepress/theme/components/Footer.vue
+++ .vitepress/theme/components/Footer.vue
@@ -24,10 +24,10 @@
           <span class="by">Powered by</span>
           <span class="name">VitePress</span>
         </a>
-        <a class="theme link" href="https://github.com/imsyy/vitepress-theme-curve" target="_blank">
+        <a class="theme link" href="https://github.com/80yearoldboy/80yearoldboy.github.io/" target="_blank">
           <span class="name">主题</span>
         </a>
-        <a class="rss link" href="https://blog.imsyy.top/rss.xml" target="_blank">
+        <a class="rss link" href="https://80yearoldboy.github.io/rss.xml" target="_blank">
           <i class="iconfont icon-rss" />
           <span class="name">订阅</span>
         </a>
